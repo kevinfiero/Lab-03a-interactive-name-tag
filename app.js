843ef5d5ee26c5848e2aa0d33a6be82c9c6c61f9
kevinfiero <kjf46@cornell.edu>
2020-09-23 17:19:42 -0700

    commit

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // import functions and grab DOM elements
+console.log('test');
 
-const 
+
+/*
+
+const inputVar = document.getElementById('name-input');
+const outputVar = document.getElementById('name-output');
+const buttonVar = document.getElementById('button');
+
+
+// set event listeners to update state and DOMs
+
+buttonVar.addEventListener('click', () => {
+    
+    console.log('test');
+
+})
+
+*/
